4182624f53595b331dfc7302391e43057d170d64
oskarew <oswa.public@gmail.com>
2024-11-25 11:49:34 +0100

    data download functions

---
+++ src/datasets/mnist.py
@@ -0,0 +1,18 @@
+# imports
+from datautils import download_mnist
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+
+#=========================================================================================#
+
+...
+
+#=========================================================================================#
+
+def main():
+    mnist_train, mnist_test = download_mnist()
+
+if __name__=='__main__':
+    main()
