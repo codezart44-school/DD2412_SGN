495e587181e32071dd976da30b1cd54577f7346b
oskarew <oswa.public@gmail.com>
2024-11-28 18:51:07 +0100

    nices formatting for data_loader.py

--- src/datasets/dataloader/data_loader.py
+++ src/datasets/dataloader/data_loader.py
@@ -7,27 +7,15 @@ from torchvision import transforms
 import torchvision
 import torchvision.transforms as transforms
 
-
 class DataLoaderCIFAR10:
 
-    # Normalization parameters - 
-    mean = [0.4914, 0.4822, 0.4465]
-    std = [0.2470, 0.2435, 0.2616]
+    # Normalization parameters
+    mean = [0.4914, 0.4822, 0.4465] # RGB means
+    std = [0.2470, 0.2435, 0.2616]  # RGB stds
 
     # Define transformations including normalization
-    transform_train = transforms.Compose([
-        # NOTE: Possible augments below....
-        #transforms.RandomCrop(32, padding=4), # Data augmentation: Random cropping 
-        #transforms.RandomHorizontalFlip(), # Data augmentation: Horizontal flipping
-
-        transforms.ToTensor(),  # Convert to PyTorch tensors and scale to [0, 1]
-        transforms.Normalize(mean, std)  # Normalize with given mean and std
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),  # Convert to PyTorch tensors and scale to [0, 1]
-        transforms.Normalize(mean, std)  # Normalize with given mean and std
-    ])
+    transform_train = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean, std)]) # can add transforms for data augmentation
+    transform_test  = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean, std)]) # NO AUGMENTATION ALLOWED
 
     @classmethod
     def get_loader(cls, transform_train=None, transform_test=None, data_dir='data/'):
@@ -39,35 +27,14 @@ class DataLoaderCIFAR10:
             transform_train = cls.transform_test
 
         # Create PyTorch datasets
-        train_dataset = torchvision.datasets.CIFAR10(
-            root=data_dir,
-            train=True,
-            download=False,
-            transform=transform_train
-        )
+        train_dataset = torchvision.datasets.CIFAR10(root=data_dir, train=True, download=False, transform=transform_train)
+        test_dataset = torchvision.datasets.CIFAR10(root=data_dir, train=False, download=False, transform=transform_test)
 
-        test_dataset = torchvision.datasets.CIFAR10(
-            root=data_dir,
-            train=False,    # Load the test set
-            download=False,
-            transform=transform_test
-        )
+        # Create PyTorch dataloaders
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=0)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=0)
 
-        train_loader = torch.utils.data.DataLoader(
-            train_dataset,
-            batch_size=128,  # Set batch size
-            shuffle=True,    # Shuffle the dataset
-            num_workers=0   # Number of subprocesses for data loading
-        )
-
-        test_loader = torch.utils.data.DataLoader(
-            test_dataset,
-            batch_size=128,
-            shuffle=False,
-            num_workers=0
-        )
-
-        return train_loader, test_loader
+        return (train_loader, test_loader)
     
 
 if __name__ == '__main__':
@@ -79,3 +46,8 @@ if __name__ == '__main__':
         print(f"Batch image shape: {images.shape}, dtype: {images.dtype}")  # Should be (64, 3, 32, 32), float32
         print(f"Batch label shape: {labels.shape}, dtype: {labels.dtype}")  # Should be (64,), int64
         break
+
+
+# SCRAP
+#transforms.RandomCrop(32, padding=4), # Data augmentation: Random cropping 
+#transforms.RandomHorizontalFlip(), # Data augmentation: Horizontal flipping
