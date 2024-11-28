ce01a2efb8219d11a4dc712547bfc74fc4b96163
oskarew <oswa.public@gmail.com>
2024-11-28 18:53:50 +0100

    removed return for test

--- src/datasets/dataloader/data_loader.py
+++ src/datasets/dataloader/data_loader.py
@@ -34,7 +34,6 @@ class DataLoaderCIFAR10:
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=0)
         test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=0)
 
-        return (train_loader, test_loader)
     
 
 if __name__ == '__main__':
